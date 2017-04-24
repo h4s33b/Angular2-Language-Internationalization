aad2b3dabe300ddefe99c5e4861a05c6cc1c2fda
h4s33b <haseeb.rehman@iunc.edu.pk>
2017-04-24 22:54:03 +0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+// app/app.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from './translate';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    moduleId: module.id,
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+
+    public translatedText: string;
+    public supportedLanguages: any[];
+    public supportedLangs : any
+    constructor(private _translate: TranslateService) { }
+
+    ngOnInit() {
+        // standing data
+        this.supportedLangs = [
+        { display: 'English', value: 'en' },
+        { display: 'Español', value: 'es' },
+        { display: '华语', value: 'zh' },
+        ];
+
+        // set current langage
+        this.selectLang('es');
+    }
+
+    isCurrentLang(lang: string) {
+        // check if the selected lang is current lang
+        return lang === this._translate.currentLang;
+    }
+
+    selectLang(lang: string) {
+        // set current lang;
+        this._translate.use(lang);
+       // this.refreshText();
+    }
+
+    // refreshText() {
+    //     // refresh translation when language change
+    //     this.translatedText = this._translate.instant('hello world');
+    // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { TRANSLATION_PROVIDERS, TranslatePipe, TranslateService }   from './translate';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslatePipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [TRANSLATION_PROVIDERS, TranslateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
